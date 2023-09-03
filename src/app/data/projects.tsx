684c41d3d63e94a8deb847e94e874e60cfa94de3
Asad Kothawala <asadkothawala97@gmail.com>
2023-09-03 11:21:42 +0530

    Added project data and removed placeholder texts

--- src/app/data/projects.tsx
+++ src/app/data/projects.tsx
@@ -4,26 +4,32 @@ export const showcase: Project[] = [
   {
     id: 1,
     img_path: "https://via.placeholder.com/300x150",
-    languages: ["React", "TypeScript"],
-    title: "Project 1",
-    discription:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    languages: ["Python", "cv2"],
+    title: "Indian Sign Language Recognizer",
+    description:
+      "A Computer Vision project created as part of University final.",
+    repo: "https://github.com/a3rium/ISL_recognizer",
+    link: "",
   },
   {
     id: 2,
     img_path: "https://via.placeholder.com/300x150",
     languages: ["React", "TypeScript"],
-    title: "Project 2",
-    discription:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    title: "Genre Classification and Sentiment Analysis of Steam Game Reviews",
+    description:
+      "An NLP project made as part of a University final to analyse and compare methods for Genre Classification and Sentiment Analysis of Steam game reviews.",
+    repo: "https://github.com/a3rium/nlp_game_reviews",
+    link: "",
   },
   {
     id: 3,
     img_path: "https://via.placeholder.com/300x150",
     languages: ["React", "TypeScript"],
-    title: "Project 3",
-    discription:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    title: "Medical Adherence Android Application",
+    description:
+      "An android application to help the vision impaired adhere to medical prescriptions build as part of a University project.",
+    repo: "https://github.com/a3rium/medical_adherence_app",
+    link: "",
   },
 ];
 
@@ -31,33 +37,64 @@ export const completed: Project[] = [
   {
     id: 1,
     img_path: "https://via.placeholder.com/300x150",
-    languages: ["React", "TypeScript"],
-    title: "Project 1",
-    discription:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    languages: ["Python", "cv2"],
+    title: "Indian Sign Language Recognizer",
+    description:
+      "A Computer Vision project created as part of University final.",
+    repo: "https://github.com/a3rium/ISL_recognizer",
+    link: "",
   },
   {
     id: 2,
     img_path: "https://via.placeholder.com/300x150",
     languages: ["React", "TypeScript"],
-    title: "Project 2",
-    discription:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    title: "Genre Classification and Sentiment Analysis of Steam Game Reviews",
+    description:
+      "An NLP project made as part of a University final to analyse and compare methods for Genre Classification and Sentiment Analysis of Steam game reviews.",
+    repo: "https://github.com/a3rium/nlp_game_reviews",
+    link: "",
   },
   {
     id: 3,
     img_path: "https://via.placeholder.com/300x150",
     languages: ["React", "TypeScript"],
-    title: "Project 3",
-    discription:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    title: "Medical Adherence Android Application",
+    description:
+      "An android application to help the vision impaired adhere to medical prescriptions build as part of a University project.",
+    repo: "https://github.com/a3rium/medical_adherence_app",
+    link: "",
   },
   {
     id: 4,
     img_path: "https://via.placeholder.com/300x150",
-    languages: ["React", "TypeScript"],
-    title: "Project 4",
-    discription:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    languages: ["SQL", "Java"],
+    title: "Rest API",
+    description:
+      "A simple Rest API using Spring Framework created as part of a University Project.",
+    repo: "https://github.com/a3rium/rest_api",
+    link: "",
+  },
+];
+
+export const play_pen: Project[] = [
+  {
+    id: 1,
+    img_path: "https://via.placeholder.com/300x150",
+    languages: ["C#"],
+    title: "Infinite Borders",
+    description:
+      "Simple 2.5D inifinite Runner game built with Unity Game Engine.",
+    repo: "https://github.com/a3rium/infinite-borders",
+    link: "",
+  },
+  {
+    id: 2,
+    img_path: "https://via.placeholder.com/300x150",
+    languages: ["TypeScript", "NextJS", "TailwindCSS"],
+    title: "Personal Portfolio",
+    description:
+      "A personal portfolio website built using Next.js and TailwindCSS",
+    repo: "https://github.com/a3rium/asadk-portfolio",
+    link: "https://asadk-portfolio.vercel.app/",
   },
 ];
