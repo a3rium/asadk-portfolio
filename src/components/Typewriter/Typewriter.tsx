ff24007c50a40eff47f734771ea33a90199254c7
Asad Kothawala <asadkothawala97@gmail.com>
2023-09-08 15:17:05 +0530

    remove debugging logs

--- src/components/Typewriter/Typewriter.tsx
+++ src/components/Typewriter/Typewriter.tsx
@@ -16,7 +16,6 @@ const Typewriter = () => {
     const changeTitle = () => {
       let timer: number;
       let newTitle: string;
-      // console.log("Last: " + lastTitle);
       do {
         if (!lastTitle) {
           const index = Math.floor(Math.random() * titles.length);
@@ -26,11 +25,9 @@ const Typewriter = () => {
           newTitle = titles[index];
         }
       } while (newTitle === lastTitle);
-      // console.log("Title: " + newTitle + " Length: " + newTitle.length);
       titleLength = newTitle.length;
       timer = titleLength * time_per_char + 250;
       setTitle(newTitle);
-      // console.log("New: " + newTitle);
       lastTitle = newTitle;
       title_picker.current = window.setTimeout(changeTitle, timer);
     };
@@ -41,29 +38,23 @@ const Typewriter = () => {
   }, []);
 
   useEffect(() => {
-    // console.log(title);
     let charsTyped: number = 0;
     let typingForward: boolean = true;
     setTypedText("");
     const typeText = () => {
       if (title) {
-        // console.log("Title Length:" + title.length);
         if (typingForward && title.length === charsTyped) {
           typingForward = false;
         }
-        // console.log(typingForward);
         if (typingForward && charsTyped < title.length) {
-          // console.log("Increasing chars");
           charsTyped = charsTyped + 1;
           setTypedText(title.slice(0, charsTyped));
           char_counter.current = window.setTimeout(typeText, 200);
         } else if (!typingForward && charsTyped != 0) {
-          // console.log("Decreasing chars");
           charsTyped = charsTyped - 1;
           setTypedText(title.slice(0, charsTyped));
           char_counter.current = window.setTimeout(typeText, 100);
         }
-        // console.log(charsTyped);
       }
     };
     typeText();
