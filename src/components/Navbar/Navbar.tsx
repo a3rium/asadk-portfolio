03449760c84f15a60ba096526c6c8bf6e02a8ac3
Asad Kothawala <asadkothawala97@gmail.com>
2023-09-08 14:47:17 +0530

    update navbar for accessibility

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,21 +9,21 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   return (
     <>
-      <nav
-        className={`menu duration-300 flex-col justify-center md:flex-row flex fixed w-full ${
-          !toggle ? `right-[-100%] top-0 bottom-0` : `right-0 top-0 bottom-0`
-        } bg-tertiary md:static`}
-      >
-        {pages.map((page) => (
-          <menu>
+      <nav>
+        <ul
+          className={`menu duration-300 flex-col justify-center md:flex-row flex fixed w-full ${
+            !toggle ? `right-[-100%] top-0 bottom-0` : `right-0 top-0 bottom-0`
+          } bg-tertiary md:static`}
+        >
+          {pages.map((page) => (
             <li className="text-primary active:text-white text-4xl md:text-lg my-4 list-none mx-2 lg:mx-4 md:my-auto ">
               <Link key={page.id} href={page.path}>
                 <span className="text-secondary font-medium">#</span>
                 {page.name}
               </Link>
             </li>
-          </menu>
-        ))}
+          ))}
+        </ul>
 
         <label
           onClick={() => {
