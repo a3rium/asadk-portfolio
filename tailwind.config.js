0f31418ed17725a68f76d3e23bedb11f4fa4f3ba
Asad Kothawala <asadkothawala97@gmail.com>
2023-09-08 15:32:47 +0530

    use 2 step animation for cursor blink

--- tailwind.config.js
+++ tailwind.config.js
@@ -44,7 +44,7 @@ module.exports = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        blink: "blink 0.7s infinite",
+        blink: "blink 0.4s steps(2) infinite",
       },
     },
   },
